fa5bc61e46a3c98d1eab532a532ea2c953c076a4
Fernando Rego do Vale <121295486+Fernando-R-Vale@users.noreply.github.com>
2023-11-23 22:29:25 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,148 @@
+from models.cliente import Cliente, NCliente
+from models.servico import Servico, NServico
+from models.agenda import Agenda, NAgenda
+import datetime
+from datetime import timedelta
+from datetime import datetime
+
+class View:
+  def cliente_inserir(nome, email, fone, senha):
+    cliente = Cliente(0, nome, email, fone, senha)
+    if nome == "" or email == "" or fone == "" or senha == "": raise ValueError("Nome, e-mail, fone ou senha vazios")
+    for obj in View.cliente_listar():
+      if obj.get_email() == email: raise ValueError("E-mail repetido")
+    NCliente.inserir(cliente)
+
+  def cliente_listar():
+    return NCliente.listar()
+  
+  def cliente_listar_id(id):
+    return NCliente.listar_id(id)
+
+  def cliente_atualizar(id, nome, email, fone, senha):
+    cliente = Cliente(id, nome, email, fone, senha)
+    if nome == "" or email == "" or fone == "" or senha == "": raise ValueError("Nome, e-mail, fone ou senha vazios")
+    for obj in View.cliente_listar():
+      if obj.get_email() == email: raise ValueError("E-mail repetido")
+    NCliente.atualizar(cliente)
+    
+  def cliente_excluir(id):
+    cliente = Cliente(id, "", "", "", "")
+    NCliente.excluir(cliente)    
+
+  def cliente_admin():
+    for cliente in View.cliente_listar():
+      if cliente.get_nome() == "admin": return
+    View.cliente_inserir("admin", "admin", "0000", "admin")  
+
+  #Avaliação
+  #def cliente_login(email, senha):
+  #  for cliente in View.cliente_listar():
+  #    if cliente.get_email() == email and cliente.get_senha() == senha:
+  #      return True
+  #  return False
+
+  def cliente_login(email, senha):
+    for cliente in View.cliente_listar():
+      if cliente.get_email() == email and cliente.get_senha() == senha:
+        return cliente
+    return None
+
+  def servico_listar():
+    return NServico.listar()
+
+  def servico_listar_id(id):
+    return NServico.listar_id(id)
+
+  def servico_inserir(descricao, valor, duracao):
+    if descricao == "": raise ValueError("Descrição vazia")
+    if valor < 0: raise ValueError("Valor negativo")
+    if duracao <= 0: raise ValueError("Duração negativa ou nula")
+    NServico.inserir(Servico(0, descricao, valor, duracao))
+
+  def servico_atualizar(id, descricao, valor, duracao):
+    if descricao == "": raise ValueError("Descrição vazia")
+    if valor < 0: raise ValueError("Valor inválido")
+    if duracao <= 0: raise ValueError("Duração negativa ou nula")
+    NServico.atualizar(Servico(id, descricao, valor, duracao))
+
+  def servico_excluir(id):
+    NServico.excluir(Servico(id, "", 0, 10))
+
+  def servico_reajustar(percentual):
+    for servico in View.servico_listar():    
+      NServico.atualizar(Servico(servico.get_id(), servico.get_descricao(), servico.get_valor() * (1 + percentual/100), servico.get_duracao()))
+
+  def agenda_listar():
+    return NAgenda.listar()
+
+  def agenda_listarhoje():
+    r = []
+    hoje = datetime.today()
+    for horario in View.agenda_listar():
+      if horario.get_confirmado() == False and horario.get_data().date() == hoje.date():
+        r.append(horario)
+    return r    
+
+  def agenda_inserir(data, confirmado, id_cliente, id_servico):
+    NAgenda.inserir(Agenda(0, data, confirmado, id_cliente, id_servico))
+
+  def agenda_atualizar(id, data, confirmado, id_cliente, id_servico):
+    NAgenda.atualizar(Agenda(id, data, confirmado, id_cliente, id_servico))
+
+  def agenda_excluir(id):
+    NAgenda.excluir(Agenda(id, "", "", 0, 0))
+
+  def agenda_abrir_agenda(data, hinicio, hfim, intervalo):
+    data_inicio = datetime.strptime(f"{data} {hinicio}", "%d/%m/%Y %H:%M")
+    data_fim = datetime.strptime(f"{data} {hfim}", "%d/%m/%Y %H:%M")
+    
+    data_atual = datetime.today()
+    
+    if data_inicio.date() < data_atual.date():
+        raise ValueError("A data de início não pode ser anterior à data atual")
+    
+    delta = timedelta(minutes=intervalo) 
+    if intervalo <= 0:
+        raise ValueError("O intervalo deve ser maior que zero")
+    
+    aux = data_inicio
+    while aux <= data_fim :
+      NAgenda.inserir(Agenda(0, aux, False, 0, 0))
+      aux = aux + delta
+
+  def editar_perfil(id, nome, email, fone, senha):
+    NCliente.atualizar(Cliente(id, nome, email, fone, senha))
+
+  def listar_horarios():
+    hoje = datetime.today()
+    uma_semana = hoje + timedelta(days=7)
+    horarios_disponiveis = []
+
+    for agenda in View.agenda_listar():
+        #Botei também para quando solicitar um agendamento, não ficar mais disponível para outro agendamento até que o adm confirme ou não.
+        if hoje.date() <= agenda.get_data().date() <= uma_semana.date() and agenda.get_confirmado() == False and agenda.get_id_cliente() == agenda.get_id_servico() == 0:
+            horarios_disponiveis.append(agenda)
+
+    return horarios_disponiveis
+
+  def periodo_informado(datainicial, datafinal, idcliente):
+    datainicial = datetime.strptime(f"{datainicial}", "%d/%m/%Y")
+    datafinal = datetime.strptime(f"{datafinal}", "%d/%m/%Y")
+    
+    periodo = []
+    
+    for horario in View.agenda_listar():
+        if horario.get_id_cliente() == idcliente:
+            if datainicial <= horario.get_data() <= datafinal:
+                periodo.append(horario)
+    
+    return periodo
+  
+  def listar_naoconfirmados():
+    nao_confirmados = []
+    for agenda in View.agenda_listar():
+      if agenda.get_confirmado() == False:
+        nao_confirmados.append(agenda)
+    
+    return nao_confirmados
